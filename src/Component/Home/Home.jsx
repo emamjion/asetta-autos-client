76b65b8f90a1d490fb72616ae2a5bd4cf8f7eabc
Sagor Ahmed <developersagorahmed@gmail.com>
2023-08-17 20:20:42 +0600

    added router setup

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            {/* add your component here  */}
+            
+        </div>
+    );
+};
+
+export default Home;
