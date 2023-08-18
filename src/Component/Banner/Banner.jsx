178ac8dee61bd48c672729ae62084430683792e2
Sagor Ahmed <developersagorahmed@gmail.com>
2023-08-18 19:32:05 +0600

    added new slider banner

---
+++ src/Component/Banner/Banner.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import css from "./Banner.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../../assets/images/Banner/banner-1.png";
+import img2 from "../../assets/images/Banner/banner-2.png";
+import img3 from "../../assets/images/Banner/banner-3.png";
+import img4 from "../../assets/images/Banner/banner-4.png";
+import img5 from "../../assets/images/Banner/banner-5.png";
+import img6 from "../../assets/images/Banner/banner-6.png";
+import img7 from "../../assets/images/Banner/banner-7.png";
+import img8 from "../../assets/images/Banner/banner-8.png";
+
+const Banner = () => {
+	return (
+		<Carousel
+			className="py-6  "
+			infiniteLoop={true}
+			autoPlay={true}
+			interval={3000}
+		>
+			<div className="">
+				<img className="object-cover h-[80vh] rounded-md" src={img1} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img2} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img3} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img4} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img5} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img6} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img7} />
+			</div>
+			<div>
+				<img className="object-cover h-[80vh] rounded-md" src={img8} />
+			</div>
+		</Carousel>
+	);
+};
+
+export default Banner;
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NewArrivals from '../NewArrivals/NewArrivals';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             {/* add your component here  */}
             {/* Banner section */}
-
+                {/*Sagor Ahmed Make Banner Section */}
+                <Banner></Banner>
             {/* About Section */}
 
 
