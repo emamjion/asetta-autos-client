2196261086aac55bc5ae632ef0a6832f710b827f
arafat717 <arafatjibon33@gmail.com>
2023-08-22 13:39:41 +0600

    login and registration before authentication

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import './Login.css'
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
     return (
-        <div className="hero min-h-screen bg-base-200 background text-white font-bold text-lg">
+        <div className="hero min-h-screen bg-base-200 background text-white font-bold text-xl">
             <div className="hero-content flex-col ">
                 <div className="card w-96 flex-shrink-0 shadow-2xl bg-transparent card-background">
                     <form className="card-body">
-                        <h1 className='text-2xl text-center font-bold'>Login</h1>
+                        <h1 className='text-2xl text-center font-bold'>Register</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered bg-transparent" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered bg-transparent" />
+                            <input type="email" placeholder="email" className="input input-bordered bg-transparent" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" placeholder="password" className="input input-bordered bg-transparent" />
+                        </div>
+                        <div className="form-control">
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <span className="label-text">Confirm Password</span>
                             </label>
+                            <input type="password" placeholder="password" className="input input-bordered bg-transparent" />
                         </div>
                         <div className="form-control mt-6">
-                            <input className="btn btn-primary bg-white border-none text-black" type="submit" value="Login" />
+                            <input className="btn btn-primary bg-white border-none text-black" type="submit" value="Register" />
                         </div>
                         <div>
-                            <p>Don't have an account? <span className=' text-[#ef1721] hover:underline'><Link to='/register'>Register</Link></span></p>
+                            <p>Don't have an account? <span className='text-[#ef1721] hover:underline'><Link to='/login'>Login</Link></span></p>
                         </div>
                     </form>
                 </div>
@@ -37,4 +45,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout/MainLayout";
 import Home from "../Component/Home/Home";
 import Login from "../Component/Login/Login";
+import Register from "../Component/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -11,12 +12,17 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
-            },
-            {
-                path: 'login',
-                element: <Login></Login>
             }
+            
         ]
+    },
+    {
+        path: 'login',
+        element: <Login></Login>
+    },
+    {
+        path: 'register',
+        element: <Register></Register>
     }
 ])
 export default router;
