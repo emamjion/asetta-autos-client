ad806ff5caea717b36e53f5e3a4d85806fde7484
arafat717 <arafatjibon33@gmail.com>
2023-08-24 12:38:57 +0600

    added captcha in login page

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -5,9 +5,9 @@ const Register = () => {
     return (
         <div className="hero min-h-screen bg-base-200 background text-white font-bold text-xl">
             <div className="hero-content flex-col ">
-                <div className="card w-96 flex-shrink-0 shadow-2xl bg-transparent card-background">
+                <div className="card w-full flex-shrink-0 shadow-2xl bg-transparent card-background">
                     <form className="card-body">
-                        <h1 className='text-2xl text-center font-bold'>Register</h1>
+                        <h1 className='text-2xl text-center font-bold text-[#ef1721]'>Register</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
@@ -33,7 +33,7 @@ const Register = () => {
                             <input type="password" placeholder="password" className="input input-bordered bg-transparent" />
                         </div>
                         <div className="form-control mt-6">
-                            <input className="btn btn-primary bg-white border-none text-black" type="submit" value="Register" />
+                            <input className="btn btn-primary border-none text-white bg-[#ef1721] hover:bg-[#181818]" type="submit" value="Register" />
                         </div>
                         <div>
                             <p>Don't have an account? <span className='text-[#ef1721] hover:underline'><Link to='/login'>Login</Link></span></p>
