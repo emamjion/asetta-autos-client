b17ed3a8dd67a485d736b115c078f94418273b95
arafat717 <arafatjibon33@gmail.com>
2023-08-28 09:30:19 +0600

    for bg-color change

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import React, {  useContext, useRef,  } from 'react';
+import React, { useContext, useRef, } from 'react';
 import { FaGoogle } from "react-icons/fa";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
@@ -7,28 +7,28 @@ import { AuthContex } from '../Providers/Authprovider';
 import app from '../firebase/firebase.config';
 const auth = getAuth(app)
 const Login = () => {
-    const {signin} = useContext(AuthContex)
+    const { signin } = useContext(AuthContex)
     let navigate = useNavigate();
     let location = useLocation();
     let from = location.state?.from?.pathname || "/";
-    
+
 
 
     const googleProvider = new GoogleAuthProvider();
 
-    const handleGoogleSignIn =() =>{
-    
+    const handleGoogleSignIn = () => {
+
         signInWithPopup(auth, googleProvider)
-        .then(result =>{
-            const user = result.user;
-            console.log(user);
-            Swal.fire('Your login is succesful')
-            navigate(from, { replace: true });
-        })
-    
-        .catch(error =>{
-            console.log('error', error.message);
-        })
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                Swal.fire('Your login is succesful')
+                navigate(from, { replace: true });
+            })
+
+            .catch(error => {
+                console.log('error', error.message);
+            })
     }
 
 
@@ -65,64 +65,61 @@ const Login = () => {
 
 
     return (
-       <div className='grid md:grid-cols-2 bg-slate-200'> 
+        <div className='grid md:grid-cols-2 bg-slate-200'>
 
-        <div>
-        <div>
-            <img className='w-full p-12 mt-4' src="https://i.ibb.co/jLRxmZV/output-onlinegiftools-1.gif" alt="" /></div>
-        </div>
+            <div>
+                <div>
+                    <img className='w-full p-12 mt-4' src="https://i.ibb.co/jLRxmZV/output-onlinegiftools-1.gif" alt="" /></div>
+            </div>
 
-<div className="bg-slate-200  hero min-h-screen bg-base-200 background text-black font-bold text-lg ">
-           
-            <div className="hero-content flex-col ">
-                <div className="card w-full flex-shrink-0 shadow-2xl bg-transparent card-background">
-                    <form onSubmit={handlesubmit} className="card-body">
+            <div className="bg-slate-200  hero min-h-screen background text-black font-bold text-lg ">
 
-                        <h1 className='text-2xl text-center font-bold text-[#ef1721]'>Login</h1>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered bg-transparent" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered bg-transparent" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control">
-                            {/* <label className="label">
+                <div className="hero-content flex-col ">
+                    <div className="card w-full flex-shrink-0 shadow-2xl bg-transparent card-background">
+                        <form onSubmit={handlesubmit} className="card-body">
+
+                            <h1 className='text-2xl text-center font-bold text-[#ef1721]'>Login</h1>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered bg-transparent" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered bg-transparent" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                {/* <label className="label">
                                 <LoadCanvasTemplate />
-<<<<<<< HEAD
                             </label> */}
-                            {/* <input type="text" ref={RefCaptcha} name='captcha' placeholder="type the captcha" className="input input-bordered bg-transparent" />
+                                {/* <input type="text" ref={RefCaptcha} name='captcha' placeholder="type the captcha" className="input input-bordered bg-transparent" />
                             <button onClick={handlevalidatecaptcha} className="btn btn-outline btn-xs mt-2">validate</button> */}
-=======
-                            </label>
-                            <input type="text" ref={RefCaptcha} name='captcha' placeholder="type the captcha" className="input input-bordered bg-transparent" />
-                            <button onClick={handlevalidatecaptcha} className="btn btn-outline btn-xs mt-2">validated</button>
->>>>>>> 968b097f784c58e3a3f5a245aee9033ffb758ae6
-                        </div>
+                                {/* </label> */}
+                                {/* <input type="text" ref={RefCaptcha} name='captcha' placeholder="type the captcha" className="input input-bordered bg-transparent" />
+                            <button className="btn btn-outline btn-xs mt-2">validated</button> */}
+                            </div>
 
-                        <div className="form-control mt-6">
-                            <input  className="btn btn-primary border-none text-white bg-[#ef1721] hover:bg-[#181818]" type="submit" value="Login" />
-                        </div>
-                        <div className="form-control mt-6">
-                           <button onClick={handleGoogleSignIn}  className="btn btn-primary border-none text-white bg-[#ef1721] hover:bg-[#181818]"><FaGoogle/>Google</button>
-                        </div>
-                        <div>
-                            <p>Don't have an account? <span className=' text-[#ef1721] hover:underline'><Link to='/register'>Register</Link></span></p>
-                        </div>
-                    </form>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary border-none text-white bg-[#ef1721] hover:bg-[#181818]" type="submit" value="Login" />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button onClick={handleGoogleSignIn} className="btn btn-primary border-none text-white bg-[#ef1721] hover:bg-[#181818]"><FaGoogle />Google</button>
+                            </div>
+                            <div>
+                                <p>Don't have an account? <span className=' text-[#ef1721] hover:underline'><Link to='/register'>Register</Link></span></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
 
-       </div>
+        </div>
     );
 };
 
