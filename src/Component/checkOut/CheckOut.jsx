59c6f46cf0feebff960bebf988372c6b37c3bf2d
Mohosin <mohosinali075@gmail.com>
2023-09-05 14:08:50 +0600

    added checkout route

---
+++ src/Component/checkOut/CheckOut.jsx
@@ -0,0 +1,10 @@
+
+const CheckOut = () => {
+    return (
+        <div className="px-4 xl:px-[140px] 2xl:px-[240px] py-10">
+            <h2 className="uppercase text-center text-xl md:text-4xl font-bold">Provide Your checkout information here</h2>
+        </div>
+    );
+};
+
+export default CheckOut;
