3ad19d5cb725197c97e47ce60fb50ccb2c5910ab
emamjion <emamjion@gmail.com>
2023-09-05 10:27:05 +0600

    I did modify about route and added more sections from emam

--- src/Component/MeetOurTeam/MeetOurTeam.jsx
+++ src/Component/MeetOurTeam/MeetOurTeam.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Tittle from '../Shared/Tittle/Tittle';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import MeetOurTeamCard from './MeetOurTeamCard';
+import Tittle from '../Shared/Tittle/Tittle';
 
 const MeetOurTeam = () => {
     const [members, setMembers] = useState([]);
@@ -13,9 +13,11 @@ const MeetOurTeam = () => {
     }, [])
     return (
         <div className='px-4 xl:px-[140px] 2xl:px-[240px] my-14 md:my-24'>
-            <Tittle topTitle={"Team"}
-                boldblackTitle={"Meet With Our"}
-                boldredTitle={"Team"}></Tittle>
+            <Tittle
+                topTitle={"Agent"} 
+                boldblackTitle={"Our"} 
+                boldredTitle={"Agent"}
+            />
             <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-12'>
                 {
                     members.map(member => <MeetOurTeamCard key={member._id} member={member}></MeetOurTeamCard>)
---
+++ src/Pages/About/OurStuff.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import demoPic from '../../assets/images/about/about-1.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { FaEnvelope, FaMobile } from 'react-icons/fa';
+
+const OurStuff = () => {
+    const [stuffs, setStuffs] = useState([]);
+    useEffect(() => {
+        fetch('/stuff.json')
+        .then(res => res.json())
+        .then(data => setStuffs(data))
+    }, [])
+    
+    return (
+        <div className='px-4 xl:px-[140px] 2xl:px-[240px] my-14 md:my-24 bg-[#f7f7f7] p-20'>
+            <div className='flex justify-center'>
+                <div className='w-1/3 p-6'>
+                    <h1 className='font-semibold text-5xl mb-6'>Meet Our Stuff</h1>
+                    <hr className='w-28 mt-2 border-[#ef1721] border' />
+                    <hr className='w-20 mt-2 border-[#ef1721] border' />
+                    <p className='mt-10 text-[#827a8e]'>
+                        We are so excited to brag about our staff! Our <br /> community is where we share expertise and diverse <br /> perspectives. You'll find a group of committed <br /> automotive professionals, each one ready to put in <br /> the time and effort to ensure your satisfaction.
+                    </p>
+                    <p className='mt-7 text-[#827a8e]'>
+                        We would love to get to know you in turn, so be sure <br /> to come see us in person!
+                    </p>
+                </div>
+                <div className='w-2/4'>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                    clickable: true,
+                }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    {
+                        stuffs.map(stuff => <SwiperSlide
+                            key={stuff.id}
+                        >
+                            <div className='flex items-center flex-col'>
+                                <img src={stuff.image} className='rounded border-8 border-[#fff]' />
+                                <div className='bg-[#fff] w-[50%] -ml-7 h-48 -mt-12 p-6 rounded'>
+                                    <h1 className='text-xl font-bold text-[#ef1721]'>{stuff.name}</h1>
+                                    <h5 className='mb-4 text-[#827a8e] font-medium'>{stuff.position}</h5>
+                                    <div className='flex items-center gap-2 mb-2 font-medium'>
+                                        <span> < FaMobile /> </span>
+                                        <span>{stuff.phone}</span>
+                                    </div>
+                                    <div className='flex items-center gap-2 font-medium'>
+                                        <span> < FaEnvelope /> </span>
+                                        <span>{stuff.email}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+                    {/* <img src={demoPic} /> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurStuff;
+
