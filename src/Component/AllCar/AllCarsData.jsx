a1f21270f0a669e9fe31bccf7bb77b1e9fa7ff14
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-09-07 15:35:07 +0600

    payment

--- src/Component/AllCar/AllCarsData.jsx
+++ src/Component/AllCar/AllCarsData.jsx
@@ -3,41 +3,46 @@ import Button from '../Button/Button';
 import { Link } from 'react-router-dom';
 import { Rating } from '@smastrom/react-rating';
 
-const AllCarsData = ({info}) => {
-    const {_id , image, price, average_rating, reviews, engine, displacement, model, make} = info;
- console.log(info);   
+const AllCarsData = ({ info }) => {
+  const { _id, image, price, average_rating, reviews, engine, displacement, model, make } = info;
 
-    return (
-        <div>
-            <div className="card card-side bg-base-100 shadow-xl grid md:grid-cols-2 ">
- <div>
-  <img className=' p-4' src={image}
-  />
-  </div>
-  <div className="card-body">
-    <h2 className="card-title">Brand: {make}</h2>
-    <h2 className="">Model: {model}</h2>
-    <h2 className=" text-[#ef1721]">Price: {price}</h2>
-    <div className='flex items-center justify-start gap-2'>
-                    <Rating
-                        style={{ maxWidth: 100 }}
-                        value={average_rating}
-                        readOnly
-                    />
-                    {average_rating}
-                    </div>
-    <h2>{engine.description}</h2>
+  return (
+    <div className='mx-auto m-4 p-4'>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl relative group 
+      ">
 
-    
-    <div className="card-actions justify-end mt-16">
-    <Link to={`/arrivals-details/${_id}`}>
-                        <Button button={'Details'} />
-                    </Link>
-    </div>
-  </div>
-</div>
+        {/* Add a wrapper for the card contents */}
+        <div className="card-content">
+        <figure className='rounded overflow-hidden hover:shadow-lg'>
+  <img src={image} alt="Shoes" className='transform hover:scale-105 transition-transform duration-200' />
+</figure>
+
+          <div className="card-body h-[235px]">
+            <div className='flex items-center justify-start gap-2'>
+              <Rating
+                style={{ maxWidth: 100 }}
+                value={average_rating}
+                readOnly
+              />
+              {average_rating}
+            </div>
+            <p className='font-bold text-xl'>Model: {model}</p>
+            <p className='font-bold text-2xl'>{engine.description}</p>
+            <p className='text-red-600 text-2xl font-bold'>${price}</p>
+          </div>
         </div>
-    );
+        
+        {/* Style the button to show/hide */}
+        <div className="card-footer opacity-0 group-hover:opacity-100 absolute left-0 w-full h-full flex items-center justify-center mt-52 transition-opacity duration-2000 ease-in-out opacity-transition">
+  <Link to={`/arrivals-details/${_id}`}>
+    <Button button={'Details'} />
+  </Link>
+</div>
+
+
+      </div>
+    </div>
+  );
 };
 
 export default AllCarsData;
