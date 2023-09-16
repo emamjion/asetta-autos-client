1966d8e2ef871248ec0e673f62ea24f61a12ae34
emamjion <emamjion@gmail.com>
2023-09-16 07:49:50 +0600

    I added car dealers details design from emam

--- src/Component/BestDealer/BestDealer.jsx
+++ src/Component/BestDealer/BestDealer.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 const BestDealer = () => {
     const [Dealers, setDealers] = useState([]);
+    console.log(Dealers);
     useEffect(() => {
         fetch('https://asetta-autos-production.up.railway.app/best-dealers/')
             .then(res => res.json())
