48e427bc1d0ff8dddfa50e86685975d57a4dd482
arafat717 <arafatjibon33@gmail.com>
2023-09-20 20:54:07 +0600

    added new data

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,25 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes.jsx";
 import Authprovider from "./Component/Providers/Authprovider";
 
-	
+
 
 import {
 	QueryClient,
 	QueryClientProvider,
-  } from '@tanstack/react-query'
-  const queryClient = new QueryClient()
+} from '@tanstack/react-query'
+import store from "./Component/Redux/store";
+import { Provider } from "react-redux";
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Authprovider>
-		<React.StrictMode>
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-    		</QueryClientProvider>	
-		</React.StrictMode>
-	</Authprovider>
+	<Provider store={store}>
+		<Authprovider>
+			<React.StrictMode>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+				</QueryClientProvider>
+			</React.StrictMode>
+		</Authprovider>
+	</Provider>
 
 );
