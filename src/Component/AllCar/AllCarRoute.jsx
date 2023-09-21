aaf64a020de35ceb29505a2d17f328017ad7f5af
emamjion <emamjion@gmail.com>
2023-09-21 08:46:43 +0600

    I added cars

--- src/Component/AllCar/AllCarRoute.jsx
+++ src/Component/AllCar/AllCarRoute.jsx
@@ -4,19 +4,28 @@ import CarContent from './CarContent';
 import CarRightSidebar from './CarRightSidebar';
 import { useContext } from 'react';
 import { AuthContex } from '../Providers/Authprovider';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const AllCarRoute = () => {
-    const { theme } = useContext(AuthContex)
+    const { theme } = useContext(AuthContex);
+    const [cars, setCars] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/new-arrivals')
+        .then(res => res.json())
+        .then(data => setCars(data))
+        
+    }, [])
     return (
-        <div className={`flex items-center my-12 px-4 xl:px-[140px] 2xl:px-[240px] mode ${theme} my-14 md:my-24`}>
+        <div className={`flex items-center gap-2 my-12 px-4 xl:px-[140px] 2xl:px-[240px] mode ${theme} my-14 md:my-24`}>
             <div className='h-screen w-[25%]'>
-                <CarLeftSidebar/>
+                <CarLeftSidebar cars={cars} setCars={setCars} />
             </div>
             <div className='border h-screen w-[50%]'>
-                <CarContent/>
+                <CarContent cars={cars} setCars={setCars} />
             </div>
             <div className='border h-screen w-[25%]'>
-                <CarRightSidebar/>
+                <CarRightSidebar cars={cars} setCars={setCars} />
             </div>
         </div>
     );
--- src/Component/AllCar/CarContent.jsx
+++ src/Component/AllCar/CarContent.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { PiHeart } from 'react-icons/pi';
 
-const CarContent = () => {
+const CarContent = ({cars}) => { 
     return (
         <div>
-            <div>
-                
-            </div>
+            {
+                cars.slice(0,3).map(car => <div
+                    key={car._id}
+                >
+                    <div>
+                        <img src={car.image} />
+                    </div>
+                    <div>
+                        <h1>{car.make} {car.model}</h1>
+                        <div>
+                            <span>< PiHeart /></span>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
