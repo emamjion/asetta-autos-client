141275c62ca10af17c1fd48a0b2d543730e0fb49
arafat717 <arafatjibon33@gmail.com>
2023-09-22 23:44:01 +0600

    for theme

--- src/Component/ReviewsCard/SwiperCard.jsx
+++ src/Component/ReviewsCard/SwiperCard.jsx
@@ -9,7 +9,7 @@ const SwiperCard = ({car}) => {
     return (
         <div className={`border mr-4 p-4 bg ${theme}`}>
             <img src={img} />
-            <div className='mt-4'>
+            <div className={`mt-4 text ${theme}`}>
                 <h2 className={`font-semibold text-2xl text ${theme}`}>{name}</h2>
                 <p className={`text-[#757575] mb-1 text ${theme}`}>{description}</p>
                 <p className={`text ${theme}`}>{review}</p>
